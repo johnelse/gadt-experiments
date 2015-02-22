f5a62422733956c64de41f0c35fd9958f6290cf6
John Else <john.else@citrix.com>
2015-02-22 17:12:46 +0000

    Tabs -> spaces
    Signed-off-by: John Else <john.else@citrix.com>

--- safelist/safelist.ml
+++ safelist/safelist.ml
@@ -8,17 +8,17 @@ type safe
 type unsafe
 
 type ('a, _) mylist =
-	| Nil : (_, unsafe) mylist
-	| Cons : 'a * ('a, _) mylist -> ('a, safe) mylist
+  | Nil : (_, unsafe) mylist
+  | Cons : 'a * ('a, _) mylist -> ('a, safe) mylist
 
 let safe_hd : ('a, safe) mylist -> 'a = function
-	| Cons (x, _) -> x
+  | Cons (x, _) -> x
 
 let _ =
-	let unsafe_list = Nil in
-	(* The next line would fail to compile:
-	let hd = safe_hd unsafe_list in
-	*)
-	let safe_list = Cons (5, unsafe_list) in
-	let hd = safe_hd safe_list in
-	Printf.printf "head = %d\n" hd
+  let unsafe_list = Nil in
+  (* The next line would fail to compile:
+  let hd = safe_hd unsafe_list in
+  *)
+  let safe_list = Cons (5, unsafe_list) in
+  let hd = safe_hd safe_list in
+  Printf.printf "head = %d\n" hd
